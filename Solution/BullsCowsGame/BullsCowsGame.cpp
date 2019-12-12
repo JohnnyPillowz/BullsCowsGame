93213fc36dd150ddece06788df3ee543d02bca83
JohnnyPillowz <szpilajan@gmail.com>
2019-12-12 21:14:28 +0100

    Lesson 22 (constexpr)

--- Solution/BullsCowsGame/BullsCowsGame.cpp
+++ Solution/BullsCowsGame/BullsCowsGame.cpp
@@ -1,4 +1,12 @@
-﻿int main(int)
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
 {
+	constexpr int WORD_LENGTH = 5;
+	cout << "Eluwina Byczki i Krufki! *u*"	<< endl;
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of? UwU\n";
+
 	return 0;
 }
